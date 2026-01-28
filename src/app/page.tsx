3d707e42675babe32909897dd912202f8ae2b177
Alejandro Gomez Curto <alejandrogomezcurto@usal.es>
2026-01-28 09:49:30 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import CatalogView from '@/features/catalog/CatalogView';
 export default function Home() {
   return (
     <main className="p-8">
-      <h1>Dulces Pétalos - Catálogo</h1>
       { <CatalogView /> }
     </main>
   );
