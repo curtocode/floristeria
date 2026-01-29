602ef5d9d553249bb1fbad7079e681c63336d92a
Alejandro Gomez Curto <alejandrogomezcurto@usal.es>
2026-01-29 14:12:18 +0100

    delete comments

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // Next.js usa process.env para leer las variables
+  // process.env para leer las variables
   baseURL: process.env.NEXT_PUBLIC_API_URL, 
   headers: {
     'Content-Type': 'application/json',
